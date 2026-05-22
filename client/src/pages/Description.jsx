d39872d590470204afe178e0edd4df013e70dbd1
Buzzalvin <buzzalvin01@gmail.com>
2026-05-22 11:15:16 +0100

    fix useRecommendations import

--- client/src/pages/Description.jsx
+++ client/src/pages/Description.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useMangaDetail } from "../hooks/useMangaDetail";
 import { useChapter } from "../hooks/useChapter";
-import { useRecommendations } from "../hooks/useRecommendation";
+import { useRecommendations } from "../hooks/useRecommendations";
 import {
   addBookmark,
   removeBookmark,
